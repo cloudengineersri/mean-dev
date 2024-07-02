3bd1000f52d3470c3bc275adc74838906d823793
cloudengineersri <cloudengineersri@gmail.com>
2024-07-02 04:10:23 +0000

    updated the script

--- mean-stack-example/01-mongo-db-docker-compose/mongo-init/init-bezkoder_db.js
+++ mean-stack-example/01-mongo-db-docker-compose/mongo-init/init-bezkoder_db.js
@@ -8,6 +8,8 @@ db.tutorials.insertOne({
     createdAt: Date(),
     updatedAt: Date()
   })
+db.tutorials.find()
 db.myCollectionName.createIndex({ "address.zip": 1 }, { unique: false });
 db.myCollectionName.insert({ "address": { "city": "Paris", "zip": "123" }, "name": "Mike", "phone": "1234" });
 db.myCollectionName.insert({ "address": { "city": "Marsel", "zip": "321" }, "name": "Helga", "phone": "4321" });
+
