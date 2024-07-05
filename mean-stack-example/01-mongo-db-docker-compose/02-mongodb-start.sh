3912ca5383aec78e10e3068fbc653a3808974f4e
cloudengineersri <cloudengineersri@gmail.com>
2024-07-05 06:16:06 +0000

    update scripts

--- mean-stack-example/01-mongo-db-docker-compose/02-mongodb-start.sh
+++ mean-stack-example/01-mongo-db-docker-compose/02-mongodb-start.sh
@@ -1,1 +1,12 @@
 docker-compose -f docker-compose.yml up -d
+mongosh --eval "printjson(db.serverStatus())"
+mongosh  --eval "db.showCollections"  "mongodb://mongo:mongo@0.0.0.0:27017/bezkoder_db"
+
+mongosh -u "mongo" -p "mongo" --host  "localhost" --port 27017 --authenticationDatabase "bezkoder_db" --eval "db.listCollections"
+
+mongosh -u "mongo" -p "mongo" --eval "db.tutorials.find()" bezkoder_db
+
+mongosh -u "mongo" -p "mongo" --host localhost --port 27017 --eval "db.tutorials.find()" bezkoder_db
+
+mongosh  -u "mongo" -p "mongo" --host localhost --port 27017  --eval "db.getCollectionNames()" bezkoder_db
+mongosh "mongodb://mongo:mongo@0.0.0.0:27017/bezkoder_db"
